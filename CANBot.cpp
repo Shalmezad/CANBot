ecfb3fb06d19ad306a4fd0c036f11e34f2844b39
Shalmezad <Shalmezad@gmail.com>
2014-01-11 10:15:43 -0500

    Initial Commit.
    Basic tank drive using 2 joysticks, 4 motors.

---
+++ CANBot.cpp
@@ -0,0 +1,50 @@
+#include "WPILib.h"
+#include "CANJaguar.h"
+
+//MOTOR CAN IDs
+const int FRONT_LEFT_MOTOR_ID = 2;
+const int BACK_LEFT_MOTOR_ID = 3;
+const int FRONT_RIGHT_MOTOR_ID = 4;
+const int BACK_RIGHT_MOTOR_ID = 5;
+
+class CANBot : public IterativeRobot
+{
+	//Joysticks (only 2 of them)
+	Joystick *leftStick, *rightStick;
+	//Jaguars
+	CANJaguar *frontLeftMotor;
+	CANJaguar *backLeftMotor;
+	CANJaguar *frontRightMotor;
+	CANJaguar *backRightMotor;
+
+public:
+	CANBot()
+	{
+		//Initialize the joysticks.
+		leftStick = new Joystick(1);
+		rightStick = new Joystick(2);
+		//Initialize the motors
+		frontLeftMotor = new CANJaguar(FRONT_LEFT_MOTOR_ID);
+		backLeftMotor = new CANJaguar(BACK_LEFT_MOTOR_ID);
+		frontRightMotor = new CANJaguar(FRONT_RIGHT_MOTOR_ID);
+		backRightMotor = new CANJaguar(BACK_RIGHT_MOTOR_ID);
+	}
+	
+	void TeleopPeriodic()
+	{
+		//declare some holder variables
+		float leftSpeed, rightSpeed;
+		//get the speeds from the joystics
+		leftSpeed = leftStick->GetY();
+		rightSpeed = rightStick->GetY();
+		//set the motor speeds.
+		frontLeftMotor -> Set(leftSpeed);
+		backLeftMotor -> Set(leftSpeed);
+		frontRightMotor -> Set(leftSpeed);
+		backRightMotor -> Set(leftSpeed);
+	}
+
+};
+
+START_ROBOT_CLASS(CANBot);
+
